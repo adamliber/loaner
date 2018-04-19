45bd6f9479bcef0be8c7aed6234d565e1cae695a
Daniyal Shafi <dshafi@usc.edu>
2018-04-19 02:18:02 -0700

    fixed the search functionality

--- NeighbourlyServer/src/Testing.java
+++ NeighbourlyServer/src/Testing.java
@@ -3,28 +3,10 @@ import java.util.ArrayList;
 public class Testing {
 	public static void main(String[] args) {
 		Database myDB = new Database();
-		//myDB.signUp("djnakuda@gmail.com", "Daniyal", "password");
-		//myDB.signUp("djnakuda@gmail.com", "Daniyal", "password");
-		//myDB.signUp("dshafi@gmail.com", "Daniyal Shafi", "password1");
-		/*myDB.signUp("Avni.barman@gmail.com", "Avni", "avni");
-		myDB.addItemToDatabase(1, "Pokemon", "", "This is a really fun game", 34.8765, -118.876757);
-		myDB.addItemToDatabase(1, "Camera", "", "This is a really camera", 34.8765, -118.876757);
-		myDB.addItemToDatabase(2, "Football", "", "This is a really good ball", 34.8765, -118.876757);
-		myDB.addItemToDatabase(2, "Camera Recorder", "", "This is a really good camera", 34.8765, -118.876757);
-		*/
-		//ArrayList<Item> myItems = myDB.searchItems(2, "camera", -150, 150, -150, 150);
-		
-		//for(int i = 0; i < myItems.size();i++)
-		//{
-		//	myItems.get(i).printItem();
-		//}
-		
-		//myDB.signUp("adam", "liber", "liber");
-		
-		//myDB.searchItemsByDistance("cat", 1, 1, 5);
-		//myDB.putUserImage();
-		//myDB.getUserImage();
-		ArrayList<Item> myItems = myDB.getAllItemsInDatabase(1.0, 1.0, 5);
+		//myDB.addItemToDatabase(1,"camera", " ", "this is the camera", 34.023334,-118.2889454);
+		//myDB.addItemToDatabase(1,"ladder", " ", "this is the ladder", 34.021605,-118.2928916);
+		//myDB.addItemToDatabase(1,"home", " ", "this is the ladder", 34.021046, -118.276473);
+		ArrayList<Item> myItems = myDB.searchItemsByDistance("cat",34.023334, -118.2889454 , 1);
 		for(int i = 0; i < myItems.size();i++)
 			{
 				myItems.get(i).printItem();
